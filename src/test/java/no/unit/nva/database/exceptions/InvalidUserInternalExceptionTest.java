23579c066180522e89a80ac462773a464ec20d8d
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-06-15 11:10:46 +0200

    NP-985: add tests for invalidUserInternalException

---
+++ src/test/java/no/unit/nva/database/exceptions/InvalidUserInternalExceptionTest.java
@@ -0,0 +1,30 @@
+package no.unit.nva.database.exceptions;
+
+import static no.unit.nva.database.exceptions.UnexpectedExceptionTest.SOME_MESSAGE;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+public class InvalidUserInternalExceptionTest {
+
+    @Test
+    public void invalidUserInternalExceptionHasConstructorWithMessage() {
+        Executable action = () -> {
+            throw new InvalidUserInternalException(SOME_MESSAGE);
+        };
+        InvalidUserInternalException exception = assertThrows(InvalidUserInternalException.class, action);
+        assertThat(exception.getMessage(), containsString(SOME_MESSAGE));
+    }
+
+    @Test
+    public void invalidUserInternalExceptionReturnsBadRequest() {
+        InvalidUserInternalException error = new InvalidUserInternalException(SOME_MESSAGE);
+        assertThat(error.statusCode(), is(equalTo(HttpStatus.SC_INTERNAL_SERVER_ERROR)));
+    }
+}
