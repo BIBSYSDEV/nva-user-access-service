3c83cb5b1e0c6817fbb23141b13864e4dd0f2c46
Orestis Gkorgkas <orestis.gorgas@gmail.com>
2020-08-14 20:48:00 +0200

    NP-1261: use SecretsReader

--- src/main/java/no/unit/nva/handlers/authorizer/LambdaAuthorizer.java
+++ src/main/java/no/unit/nva/handlers/authorizer/LambdaAuthorizer.java
@@ -1,17 +1,12 @@
 package no.unit.nva.handlers.authorizer;
 
-import static nva.commons.utils.attempt.Try.attempt;
-
 import com.amazonaws.services.secretsmanager.AWSSecretsManager;
 import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
-import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
-import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
-import com.fasterxml.jackson.databind.JsonNode;
 import nva.commons.exceptions.ForbiddenException;
 import nva.commons.handlers.authentication.ServiceAuthorizerHandler;
 import nva.commons.utils.Environment;
-import nva.commons.utils.JsonUtils;
-import nva.commons.utils.attempt.Failure;
+import nva.commons.utils.JacocoGenerated;
+import nva.commons.utils.aws.SecretsReader;
 
 public class LambdaAuthorizer extends ServiceAuthorizerHandler {
 
@@ -20,6 +15,7 @@ public class LambdaAuthorizer extends ServiceAuthorizerHandler {
     public static final String AWS_SECRET_KEY_ENV_VAR = "API_SECRET_KEY";
     private final AWSSecretsManager awsSecretsManager;
 
+    @JacocoGenerated
     public LambdaAuthorizer() {
         this(newAwsSecretsManager(), new Environment());
     }
@@ -37,28 +33,11 @@ public class LambdaAuthorizer extends ServiceAuthorizerHandler {
     @Override
     protected String fetchSecret() throws ForbiddenException {
         final String secretName = environment.readEnv(AWS_SECRET_NAME_ENV_VAR);
-
-        GetSecretValueResult getSecretResult = awsSecretsManager.getSecretValue(
-            new GetSecretValueRequest().withSecretId(secretName));
-        return extractApiKey(getSecretResult);
-    }
-
-    private String extractApiKey(GetSecretValueResult getSecretResult)
-        throws ForbiddenException {
-
         final String secretKey = environment.readEnv(AWS_SECRET_KEY_ENV_VAR);
-        String secretString = getSecretResult.getSecretString();
-        String secretValue = attempt(() -> JsonUtils.objectMapper.readTree(secretString))
-            .map(secretJson -> secretJson.get(secretKey))
-            .map(JsonNode::textValue)
-            .orElseThrow(this::logErrorAndThrowException);
-        return secretValue;
+        SecretsReader secretsReader = new SecretsReader(awsSecretsManager);
+        return secretsReader.fetchSecret(secretName, secretKey);
     }
 
-    private <I> ForbiddenException logErrorAndThrowException(Failure<I> failure) {
-        logger.error(failure.getException().getMessage(), failure.getException());
-        return new ForbiddenException();
-    }
 
     private static AWSSecretsManager newAwsSecretsManager() {
         return AWSSecretsManagerClientBuilder.defaultClient();
